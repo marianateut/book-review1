2e5f53d81fc0fb02c762583dda9774432b8205cd
Mariana Teut <mary_marianailea@yahoo.com>
2020-01-07 23:35:43 +0200

    Initial commit

---
+++ src/main/java/org/fasttrackit/bookreview1/BookReview1Application.java
@@ -0,0 +1,13 @@
+package org.fasttrackit.bookreview1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookReview1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookReview1Application.class, args);
+	}
+
+}
